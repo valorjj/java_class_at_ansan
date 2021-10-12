3801f6b3e403c5366fc6359907e073726072c781
505 <505@DESKTOP-B9HIA7V>
2021-10-12 18:03:48 +0900

    commit

--- Ezen_0929/src/CafeKiosk/coffee.java
+++ Ezen_0929/src/CafeKiosk/coffee.java
@@ -2,4 +2,66 @@ package CafeKiosk;
 
 public class coffee {
 
+	String coffee_name;
+	int coffee_price;
+	int coffee_inventory;
+	String coffee_hot;
+	String coffee_size;
+
+	public coffee(String coffee_name, int coffee_price, int coffee_inventory, String coffee_hot, String coffee_size) {
+		super();
+		this.coffee_name = coffee_name;
+		this.coffee_price = coffee_price;
+		this.coffee_inventory = coffee_inventory;
+		this.coffee_hot = coffee_hot;
+		this.coffee_size = coffee_size;
+	}
+
+	public coffee(String coffee_name, int coffee_price, int coffee_inventory) {
+		super();
+		this.coffee_name = coffee_name;
+		this.coffee_price = coffee_price;
+		this.coffee_inventory = coffee_inventory;
+	}
+
+	public String getCoffee_name() {
+		return coffee_name;
+	}
+
+	public void setCoffee_name(String coffee_name) {
+		this.coffee_name = coffee_name;
+	}
+
+	public int getCoffee_price() {
+		return coffee_price;
+	}
+
+	public void setCoffee_price(int coffee_price) {
+		this.coffee_price = coffee_price;
+	}
+
+	public int getCoffee_inventory() {
+		return coffee_inventory;
+	}
+
+	public void setCoffee_inventory(int coffee_inventory) {
+		this.coffee_inventory = coffee_inventory;
+	}
+
+	public String getCoffee_hot() {
+		return coffee_hot;
+	}
+
+	public void setCoffee_hot(String coffee_hot) {
+		this.coffee_hot = coffee_hot;
+	}
+
+	public String getCoffee_size() {
+		return coffee_size;
+	}
+
+	public void setCoffee_size(String coffee_size) {
+		this.coffee_size = coffee_size;
+	}
+
 }
--- Ezen_0929/src/CafeKiosk/dessert.java
+++ Ezen_0929/src/CafeKiosk/dessert.java
@@ -2,4 +2,39 @@ package CafeKiosk;
 
 public class dessert {
 
+	String dessert_name;
+	int dessert_price;
+	int dessert_inventory;
+
+	public dessert(String dessert_name, int dessert_price, int dessert_inventory) {
+		super();
+		this.dessert_name = dessert_name;
+		this.dessert_price = dessert_price;
+		this.dessert_inventory = dessert_inventory;
+	}
+
+	public String getDessert_name() {
+		return dessert_name;
+	}
+
+	public void setDessert_name(String dessert_name) {
+		this.dessert_name = dessert_name;
+	}
+
+	public int getDessert_price() {
+		return dessert_price;
+	}
+
+	public void setDessert_price(int dessert_price) {
+		this.dessert_price = dessert_price;
+	}
+
+	public int getDessert_inventory() {
+		return dessert_inventory;
+	}
+
+	public void setDessert_inventory(int dessert_inventory) {
+		this.dessert_inventory = dessert_inventory;
+	}
+
 }
